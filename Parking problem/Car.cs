9558cd696c9c30f5ee0a2718688ca5f3b9980fff
Yanis Hermassi <yanis.hermassi@gmail.com>
2021-02-21 15:35:03 +0000

    cleaning code

---
+++ Parking problem/Car.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Parking_problem
+{
+    public class Car
+    {
+        public int Hours { get; set; }
+
+        public double Charges = 2;
+        
+        public string Name { get; set; }
+        
+        public Car(string _name, int _hours)
+        {
+            Hours = _hours;
+            Name = _name;
+        }
+
+        public double CalculateCharges()
+        {
+            if (Hours < 3)
+                return Charges;
+            if (Hours == 24)
+            {
+                Charges = 10;
+                return Charges;
+            }
+            Charges += 0.5 * Hours;
+            return Charges;
+        }
+
+        public void CarDisplay()
+        {
+            Console.WriteLine("Name: {0}", Name);
+            CalculateCharges();
+            Console.WriteLine("Hours: {0} h", Hours);
+            Console.WriteLine("Charges: {0} €", Charges);
+            Console.WriteLine("--------------");
+            Charges = 2;
+        }
+    }
+}
--- Parking problem/Carpark.cs
+++ Parking problem/Carpark.cs
@@ -33,42 +33,4 @@ namespace Parking_problem
             Console.WriteLine("Yesterday Receipt: {0} €", TotalCharges);
         }
     }
-
-    public class Car
-    {
-        public int Hours { get; set; }
-
-        public double Charges = 2;
-        
-        public string Name { get; set; }
-        
-        public Car(string _name, int _hours)
-        {
-            Hours = _hours;
-            Name = _name;
-        }
-
-        public double CalculateCharges()
-        {
-            if (Hours < 3)
-                return Charges;
-            if (Hours == 24)
-            {
-                Charges = 10;
-                return Charges;
-            }
-            Charges += 0.5 * Hours;
-            return Charges;
-        }
-
-        public void CarDisplay()
-        {
-            Console.WriteLine("Name: {0}", Name);
-            CalculateCharges();
-            Console.WriteLine("Hours: {0} h", Hours);
-            Console.WriteLine("Charges: {0} €", Charges);
-            Console.WriteLine("--------------");
-            Charges = 2;
-        }
-    }
 }
